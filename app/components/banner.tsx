4757f13756a6883778e00ad9bd5cec2ec69aff52
learnuidev <learnuidev@gmail.com>
2025-03-26 15:44:36 -0400

    Added chadcn

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,8 +1,10 @@
 export const Banner = () => {
   return (
     <section>
-      <h1>Masala</h1>
-      <p>AI based calorie + weight tracking app</p>
+      <h1 className="text-center mt-24 text-4xl font-bold">Masala</h1>
+      <p className="text-center mt-4 text-xl">
+        AI based calorie + weight tracking app
+      </p>
     </section>
   );
 };

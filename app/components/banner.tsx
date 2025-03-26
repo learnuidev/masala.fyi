cd3322e82ab8c019bf97a97ca9c4bc1d40b27f68
learnuidev <learnuidev@gmail.com>
2025-03-26 16:08:13 -0400

    added get started button

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,14 +1,21 @@
 "use client";
 import { useTranslation } from "@/libs/i18n-next/use-translation";
+import Link from "next/link";
 
 export const Banner = () => {
   const { t } = useTranslation(["banner"]);
   return (
     <section className="max-w-md m-auto flex justify-center items-center flex-col">
-      <h1 className="text-center mt-24 text-4xl font-bold">
+      <h1 className="text-center mt-24 lg:text-7xl text-4xl font-bold lowercase">
         {t("banner:title")}
       </h1>
-      <p className="text-center mt-4 text-xl">{t("banner:description")}</p>
+      <p className="text-center mt-4 text-xl lg:text-2xl dark:text-gray-300 text-gray-700 font-light">
+        {t("banner:description")}
+      </p>
+
+      <Link href="/app" className="mt-8 border-2 px-2 py-1">
+        {t("banner:get.started")}
+      </Link>
     </section>
   );
 };
